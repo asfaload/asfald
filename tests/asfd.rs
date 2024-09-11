206cc4315e610c39dbb6cf66f61953f5f7670b9d
Bauduin Raphaël <raphael@asfaload.com>
2024-09-11 14:23:50 +0200

    test: add curl request to external server

--- tests/asfd.rs
+++ tests/asfd.rs
@@ -1,5 +1,11 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use std::process::Command;
+
 #[test]
 fn it_works() {
-    let result = 2 + 2;
-    assert_eq!(result, 4);
+    let mut cmd = Command::new("/usr/bin/curl");
+    cmd.arg("http://localhost:3030/data/valid/the_file.txt");
+    println!("will assert success");
+    cmd.spawn().unwrap();
+    cmd.assert().success();
 }
