e364cff234f1caad9eb1290eb8c4690c92ec0551
Bauduin Raphaël <raphael@asfaload.com>
2024-09-11 14:28:03 +0200

    test: first asfd test with valid file

--- tests/asfd.rs
+++ tests/asfd.rs
@@ -2,10 +2,9 @@ use assert_cmd::prelude::*; // Add methods on commands
 use std::process::Command;
 
 #[test]
-fn it_works() {
-    let mut cmd = Command::new("/usr/bin/curl");
-    cmd.arg("http://localhost:3030/data/valid/the_file.txt");
-    println!("will assert success");
+fn file_with_valid_checksum() {
+    let mut cmd = Command::new("target/debug/asfd");
+    cmd.arg("http://localhost:3030/valid/the_file.txt");
     cmd.spawn().unwrap();
     cmd.assert().success();
 }
