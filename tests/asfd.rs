6dafcdeb3e3dea1d2eb1c804510a5d0c53857e90
Bauduin Raphaël <raphael@asfaload.com>
2024-09-12 12:43:02 +0200

    test: cover -p with fullpath pattern

--- tests/asfd.rs
+++ tests/asfd.rs
@@ -86,6 +86,31 @@ fn file_with_valid_checksum_p_url() {
 }
 
 #[test]
+// Test -p flag
+fn file_with_valid_checksum_p_fullpath() {
+    // Create out dedicated directory
+    let dir: PathBuf = testdir!();
+    let mut cmd = Command::new("target/debug/asfd");
+    // Download the file to our dedicated directory
+    cmd.arg("-o");
+    cmd.arg(dir.join("the_file.txt"));
+    // Specify the url where to get the checksums file
+    cmd.arg("-p");
+    cmd.arg("${fullpath}.sha256");
+    // Get the file from its server
+    cmd.arg(url("/valid_suffix/the_file.txt"));
+
+    cmd.assert()
+        .success()
+        .stdout(contains("Checksum file found !"))
+        .stdout(contains("File\'s checksum is valid !"));
+
+    let is_file_pred = is_file();
+    // Check the original filename is not present
+    assert!(is_file_pred.eval(Path::new(&dir.join("the_file.txt"))));
+}
+
+#[test]
 // Test -q flag
 fn file_with_valid_checksum_q() {
     // Create out dedicated directory
