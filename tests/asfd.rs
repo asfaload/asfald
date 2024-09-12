4dd893933ea6b69f24df228fc9f8a18bfd9e79b7
Bauduin Raphaël <raphael@asfaload.com>
2024-09-12 09:14:23 +0200

    test: test stdout and file presence

--- tests/asfd.rs
+++ tests/asfd.rs
@@ -1,4 +1,6 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*;
+use predicates::str::contains;
 use std::process::Command;
 
 const HTTP_SERVER: &str = "http://localhost:9988";
@@ -9,17 +11,29 @@ fn url(path: &str) -> String {
 }
 
 #[test]
+// Test successful download without any flag
 fn file_with_valid_checksum() {
     let mut cmd = Command::new("target/debug/asfd");
     cmd.arg(url("/valid/the_file.txt"));
     // spawn will display the output of the command
     //cmd.spawn().unwrap();
-    cmd.assert().success();
+    cmd.assert()
+        .success()
+        .stdout(contains("Checksum file found !"))
+        .stdout(contains("File\'s checksum is valid !"));
+
+    let is_file_pred = is_file();
+    assert!(is_file_pred.eval(Path::new("./the_file.txt")));
+    let _ = std::fs::remove_file("./the_file.txt");
 }
 
 #[test]
+// File downloaded is present in checksums file, but the checksum is different
 fn file_with_invalid_checksum() {
     let mut cmd = Command::new("target/debug/asfd");
     cmd.arg(url("/invalid_checksum/the_file.txt"));
-    cmd.assert().failure();
+    cmd.assert()
+        .failure()
+        .stdout(contains("Checksum file found !"))
+        .stdout(contains("File\'s checksum is invalid !"));
 }
