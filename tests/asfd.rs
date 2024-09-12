488a4aefec5b956e4b2bef2f9519794b22ab16d0
Bauduin Raphaël <raphael@asfaload.com>
2024-09-12 09:49:42 +0200

    test: cover -p with full url to checksums file

--- tests/asfd.rs
+++ tests/asfd.rs
@@ -7,11 +7,16 @@ use std::path::PathBuf;
 use std::process::Command;
 use testdir::testdir;
 
-const HTTP_SERVER: &str = "http://localhost:9988";
+const HTTP_HOST: &str = "http://localhost";
 
 // Helper to return the furl url of the test file to be downloaded
 fn url(path: &str) -> String {
-    format!("{HTTP_SERVER}{path}")
+    format!("{HTTP_HOST}:9988{path}")
+}
+
+// Helper to return the url of the path for the second test server
+fn snd_url(path: &str) -> String {
+    format!("{HTTP_HOST}:9989{path}")
 }
 
 #[test]
@@ -53,6 +58,31 @@ fn file_with_valid_checksum_o() {
     assert!(!is_file_pred.eval(Path::new(&dir.join("the_file.txt"))));
     // Check the file was downloaded in the requested location
     assert!(is_file_pred.eval(Path::new(&dir.join("the_local_file.txt"))));
+}
+
+#[test]
+// Test -p flag
+fn file_with_valid_checksum_p_url() {
+    // Create out dedicated directory
+    let dir: PathBuf = testdir!();
+    let mut cmd = Command::new("target/debug/asfd");
+    // Download the file to our dedicated directory
+    cmd.arg("-o");
+    cmd.arg(dir.join("the_file.txt"));
+    // Specify the url where to get the checksums file
+    cmd.arg("-p");
+    cmd.arg(snd_url("/remote_checksum/checksums.txt"));
+    // Get the file from its server
+    cmd.arg(url("/remote_checksum/the_file.txt"));
+
+    cmd.assert()
+        .success()
+        .stdout(contains("Checksum file found !"))
+        .stdout(contains("File\'s checksum is valid !"));
+
+    let is_file_pred = is_file();
+    // Check the original filename is not present
+    assert!(is_file_pred.eval(Path::new(&dir.join("the_file.txt"))));
 }
 
 #[test]
